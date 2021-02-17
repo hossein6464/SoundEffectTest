7f54a9449467f9ea2212a2b61228be6b84761bff
hossein6464 <hossein.hajisoleimani@gmail.com>
2021-02-16 23:57:57 -0500

    Initial commit

---
+++ app/src/main/java/diana/soleil/soundeffecttest/MainActivity.java
@@ -0,0 +1,27 @@
+package diana.soleil.soundeffecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void playSound(View view) {
+        mediaPlayer.start();
+    }
+    public void pauseSound(View view) {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.rain);
+    }
+}
